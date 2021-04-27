33909164bf63013dbf66bcdc8227ca8a1cc1f424
oddjree <audrey@dubler.org>
2021-04-26 21:27:46 -0500

    changed audio

--- js/audio.js
+++ js/audio.js
@@ -2,7 +2,7 @@ function music(){
 	//<audio></audio>
 	let intro = document.createElement("audio")
 	//<audio src="audio/seashanty2.mp3"></audio>
-	//intro.setAttribute('src', 'audio/seashanty2.mp3')
+	intro.setAttribute('src', 'audio/seashanty3.mp3')
 	document.body.appendChild(intro)
 	intro.play()
 }
